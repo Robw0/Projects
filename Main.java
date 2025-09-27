f30436406a86cb17c0a0b53d9d0ffbbff4ccc429
Robw0 <robbiewatters55@gmail.com>
2025-09-27 16:35:56 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,14 +5,22 @@ import java.util.Iterator;
 
 class Lab2 {
   public void linkedList() {
+    // 1. Create a LinkedList<String> object called progLanguages
     LinkedList<String> progLanguages = new LinkedList<>();
+
+    // 2. Add elements to progLanguages: "Java", "Python", "JavaScript", "C++"
     progLanguages.add("Java");
     progLanguages.add("Python");
     progLanguages.add("JavaScript");
     progLanguages.add("C++");
+
+    // 3. Remove the element "C++" from the list using .remove()
     progLanguages.remove("C++");
+
+    // 4. Add an element "HTML" at index 2.
     progLanguages.add(2, "HTML");
 
+    // 5. Iterate over progLanguages and use println() to output each element
     Iterator<String> iterator = progLanguages.iterator();
     while (iterator.hasNext()) {
       System.out.println(iterator.next());
@@ -20,38 +28,61 @@ class Lab2 {
   }
 
   public void queue() {
+    // 6. Create a Queue<String> called q
     Queue<String> q = new LinkedList<>();
+
+    // 7. Add 5 first names to q.
     q.add("Alice");
     q.add("Bob");
     q.add("Charlie");
     q.add("Diana");
     q.add("Ethan");
 
+    // 8. Uncomment the following line
     System.out.println("Elements of queue: " + q);
 
+    // 9. Remove the head of the queue and assign it to a String variable removedElement.
     String removedElement = q.poll();
     System.out.println("Removed element: " + removedElement);
 
+    // 10. View the head of the queue using peek(). Output it's value.
     System.out.println("Head of queue: " + q.peek());
 
+    // 11. Using for(String element : q), output all of the values in the queue
     for (String element : q) {
       System.out.println(element);
     }
   }
 
   public void stack() {
+    // 12. Create a Stack<String> called bookStack
     Stack<String> bookStack = new Stack<>();
+
+    // 13. Push the following book titles onto bookStack: "Clean Code", "Design Patterns", "Pragmatic Programmer"
     bookStack.push("Clean Code");
     bookStack.push("Design Patterns");
     bookStack.push("Pragmatic Programmer");
 
+    // 14. pop() 1 book off the stack. Display it's value
     System.out.println("Popped book: " + bookStack.pop());
+
+    // 15. Use the peek() method to view the top book on the stack
     System.out.println("Top book: " + bookStack.peek());
 
+    // 16. push() "Web DB Technologies" onto the stack
     bookStack.push("Web DB Technologies");
+
+    // 17. Use the peek() method to view the top book on the stack
     System.out.println("Top book after push: " + bookStack.peek());
+
+    // 18. Search for "Design Patterns" in the stack. Display the results of the search.
     System.out.println("Position of 'Design Patterns': " + bookStack.search("Design Patterns"));
+
+    // 19. Call empty(). Output the results
     System.out.println("Is stack empty? " + bookStack.empty());
+
+    // 20. Print the titles of all of the books on the stack
     System.out.println("Books on the stack: " + bookStack);
   }
 }
+
